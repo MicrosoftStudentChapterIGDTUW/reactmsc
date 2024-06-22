5050ac254f06264c4d6ef0d48896e504b2e727dd
Violetcv <you@example.com>
2024-06-22 15:27:34 +0530

    feat: mentee-btn gtm

--- src/pages/Bootcamp24/assets/CallMenteesBtn.js
+++ src/pages/Bootcamp24/assets/CallMenteesBtn.js
@@ -4,11 +4,14 @@ const CallForMenteesButton = () => {
   return (
     <div>
       <a
-        href="https://example.com/form"
+        href="https://forms.office.com/r/dSDi130fvH"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button className="bg-logo-blue1 mb-8 text-logo-white py-4 px-8 rounded-lg text-xl hover:bg-logo-blue3">
+        <button
+          id="callForMenteesButton"
+          className="bg-logo-blue1 mb-8 text-logo-white py-4 px-8 rounded-lg text-xl hover:bg-logo-blue3"
+        >
           Call for Mentees
         </button>
       </a>
