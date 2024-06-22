85719fff280af694cbd944d82b4e9f5d8785ad4c
Violetcv <you@example.com>
2024-06-22 11:03:07 +0530

    feat: incorporate tailwind

--- src/pages/prizes/Prize.js
+++ src/pages/prizes/Prize.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import './prize.css'
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+// import './prize.css' // Keep this if you still need the @font-face definition
 
-function Prize({pos,img,amt}) {
+function Prize({ pos, img, amt }) {
   return (
-    <div className='prize'>
-        <div className="pos">
-            <h1>{pos}</h1>
-        </div>
-        <div className="img">
-            <img src={img} alt="" />
-        </div>
-        <h3 style={{paddingBottom:"-100px"}}>Cash Prize:</h3>
-        <div className="amt">
-            <h3 ><CurrencyRupeeIcon />{amt}</h3>
-        </div>
+    <div className="border-2 border-whitesmoke rounded-lg h-96 w-80 text-white bg-gray-400 bg-opacity-30 font-bold mb-20 transition-transform duration-200 ease-in-out hover:scale-110">
+      <div className="text-blanchedalmond pt-5 text-4xl">
+        <h1>{pos}</h1>
+      </div>
+      <div className="pb-2">
+        <img src={img} alt="" className="h-52" />
+      </div>
+      <h3 className="pb-2">Cash Prize:</h3>
+      <div className="flex items-center justify-center pb-2">
+        <h3 className="flex items-center"><CurrencyRupeeIcon />{amt}</h3>
+      </div>
     </div>
   )
 }
 
-export default Prize
+export default Prize;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
       colors: {
         logo: {
           blue1: "#1B2F4F",
