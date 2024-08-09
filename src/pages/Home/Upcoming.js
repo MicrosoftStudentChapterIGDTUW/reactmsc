307e8aa1b1e386b1c88484c8bf92ed99a8ff6792
urvashi2004 <uy2204@gmail.com>
2024-08-09 22:01:39 +0530

    Added href to upcoming bootcamp23

--- src/pages/Home/Upcoming.js
+++ src/pages/Home/Upcoming.js
@@ -6,6 +6,7 @@ function Upcoming() {
     <>
       <div className="upcomingsection" data-aos="fade-up">
         <Hovercard
+          href="/bootcamp24"
           img="https://res.cloudinary.com/duxmh9dws/image/upload/v1694947654/bootcamp_modal_cauj6h.jpg"
           content="The Summer Bootcamp aimed to empower participants with hands-on learning experiences in diverse domains, fostering skills in Web Development, App Development, AR/VR, AI/ML, Cybersecurity, Web3, Data Analytics and Product Management."
           name="Bootcamp '24"
