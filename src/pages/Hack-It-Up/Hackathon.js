728baafeb7324ab5f224529616f3fffbfba28c79
urvashi2004 <uy2204@gmail.com>
2025-01-27 22:01:40 +0530

    Tailwind added, Work to continue on Submission

--- src/pages/Hack-It-Up/Hackathon.js
+++ src/pages/Hack-It-Up/Hackathon.js
@@ -1,16 +1,26 @@
-// import { Link } from "react-router-dom";
-import TeamRegistrationForm from "./RegistrationForm";
-import HackathonSubmission from "./SubmissionForm";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function HackItUp() {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <div>Hello</div>
-      <button>registration</button>
-      <TeamRegistrationForm/>
-      <button>submission</button>
-      <HackathonSubmission/>
-    </>
+    <div className="p-4">
+      <div className="text-4xl font-bold mb-4">Hack - It - Up</div>
+      <div
+        onClick={() => navigate('/hackitup/registration')}
+        className="cursor-pointer p-4 bg-blue-600 hover:bg-gray-300 rounded mb-2 text-xl"
+      >
+        Registration
+      </div>
+      <div
+        onClick={() => navigate('/hackitup/submission')}
+        className="cursor-pointer p-4 bg-blue-600 hover:bg-gray-300 rounded text-xl"
+      >
+        Submission
+      </div>
+    </div>
   );
 }
+
 export default HackItUp;
