1b7f86478d63385d5e540ee88fce3f3f9c58aeff
Violetcv <you@example.com>
2025-01-28 17:58:26 +0530

    home page img

--- src/pages/Home/Head.js
+++ src/pages/Home/Head.js
@@ -7,34 +7,33 @@ function Head() {
   return (
     <div className="w-screen mx-auto inline-block">
       <div className="inline-block w-full h-auto bg-transparent rounded-lg relative">
-      <div>
+        <div>
           <img
-            src="./images/HACK-IT-UP.jpg"
+            src="./images/HACK-IT-UP-HomePage-Mobile.jpg"
             alt="mobile version"
             className="absolute md:hidden" // hidden on medium screens and up, visible below
           />
 
           {/* Visible on larger screens, hidden on mobile */}
           <img
-            src="./images/HACK_IT_UP.jpg"
+            src="./images/HACK-IT-UP-HomePage.jpg"
             alt="desktop version"
             className="block" // visible on medium screens and up, hidden below
           />
         </div>
+      </div>
 
-        <div
-          onClick={() => navigate("/hackitup")}
-          className="absolute top-1/2 left-[25%] transform -translate-x-1/2 -translate-y-1/2
+      <div
+        onClick={() => navigate("/hackitup")}
+        className="mt-20 absolute top-1/2 left-[25%] transform -translate-x-1/2 -translate-y-1/2
           cursor-pointer p-4 bg-blue-600 hover:bg-blue-900 rounded-lg
           text-white text-xl transition duration-300 ease-in-out
           md:p-3 md:text-lg
-          sm:left-1/2 sm:p-2 sm:text-sm sm:w-auto sm:text-center"
-        
-          // data-aos="fade-up"
-          // data-aos-duration="200"
-        >
-          Know More!
-        </div>
+          sm:left-1/2 sm:p-2 sm:text-sm sm:w-auto sm:text-center sm:mt-20"
+        data-aos="fade-left"
+        data-aos-duration="200"
+      >
+        Know More!
       </div>
 
       <div
@@ -42,9 +41,9 @@ function Head() {
         data-aos="fade-left"
         data-aos-duration="500"
       >
-        <h1 className="">
+        <div className="text-center text-[#1B2F4F] text-5xl mt-20 sm:mt-12">
           Microsoft Student Chapter IGDTUW
-        </h1>
+        </div>
         <p className="text-lg mb-8 mt-8 font-medium sm:mt-8">
           We aim to equip the students with the tools that enable them to
           perform the best in the corporate environment right from their
