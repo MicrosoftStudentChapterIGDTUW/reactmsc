2c2a8faecfca7aa437b4e88997e66d5619f6f7b2
urvashi2004 <uy2204@gmail.com>
2025-02-01 01:23:36 +0530

    changes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBLWWtX8Do3wZ6jYXUcQ-1q_vADu8b1nuA",
+    authDomain: "msc-igdtuw-2024.firebaseapp.com",
+    projectId: "msc-igdtuw-2024",
+    storageBucket: "msc-igdtuw-2024.firebasestorage.app",
+    messagingSenderId: "168763768935",
+    appId: "1:168763768935:web:17e0aa7d5e13d8f8fcd4c2",
+    measurementId: "G-0S37Y5WWNW"
 };
 
 const app = initializeApp(firebaseConfig);
